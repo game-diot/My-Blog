9c79645f139ebddc077338feac1557888a81a542
张泽洁 <3296592663@qq.com>
2025-07-22 16:37:32 +0800

    更换部署
    更换部署

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/My-Blog/", // 关键配置：匹配你的仓库名
+  build: {
+    outDir: "dist", // 确保构建输出到 dist 文件夹（默认就是 dist，可省略）
+    emptyOutDir: true, // 构建前清空输出目录
+  },
 });
